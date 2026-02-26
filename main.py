90d8b6932018549b4802034e7fe7818d141a3d1c
MaximB0nd <sersim11@yandex.ru>
2026-02-26 14:37:16 +0300

    refactor: add func generate_random_list and use it in main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
 
+def generate_random_list(count, min_value, max_value):
+    return [random.randint(min_value, max_value) for _ in range(count)]
+
 def main():
     count = 12
     min_value = 5
     max_value = 200
 
-    numbers = []
-    for _ in range(count):
-        numbers.append(random.randint(min_value, max_value))
+    numbers = generate_random_list(count, min_value, max_value)
 
     print(numbers)
 
